5db5f002527209fb3cec2ff1659a14f37c7a0511
Adam Haley <adamhaley@gmail.com>
2012-07-10 15:47:09 -0700

    updated READAME, cleaned up and refactored clock.js a little.

--- clock.js
+++ clock.js
@@ -13,29 +13,29 @@ window.onload = function(e){
 	, s
 	;
 
-	// Calculate starting position for hands
-
+	// Get current time
 	now = new Date();
 
 	seconds = now.getSeconds();
 	minutes = now.getMinutes();
 	hours = now.getHours();
 
+	// calculate angles of hands
 	sDeg = seconds * 6;
 	mDeg = minutes * 6;
 	hDeg = hours * 30;
 
+	// get clock DOM
 	secondHand = document.getElementById('second-hand');
 	minuteHand = document.getElementById('minute-hand');
 	hourHand = document.getElementById('hour-hand');
 
-
-	
 	// set all the hands to the current time
 	secondHand.style.webkitTransform = "rotate(" + sDeg + "deg)";
 	minuteHand.style.webkitTransform = "rotate(" + mDeg + "deg)";
 	hourHand.style.webkitTransform = "rotate(" + hDeg + "deg)";
 
+	// define runClock function
 	s = 0;
 	var runClock = function(){
 
@@ -51,16 +51,12 @@ window.onload = function(e){
 			// console.log('BAM!!!  minute.');
 
 			if((mDeg % 360) === 0){
-				hDeg += 6;
+				hDeg += 30;
 				hourHand.style.webkitTransform = "rotate(" + hDeg + "deg)";
 				// console.log('BAM!!!  hour.');
 			}
-
 		}
 	}
-
+	// run the clock
 	setInterval(runClock,1000); 
-
-
-
 }
